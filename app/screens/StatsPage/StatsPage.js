83676a686f0a3fc310a2150312ed3eb56c1f40af
ernsngr <ernsngrsocial@gmail.com>
2025-09-22 18:28:43 +0300

    Veritabanı oluşturuldu ekle sil fonksiyonları eklendi

--- app/screens/StatsPage/StatsPage.js
+++ app/screens/StatsPage/StatsPage.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useCallback, useState} from 'react';
+import { useFocusEffect } from '@react-navigation/native';
 import { ScrollView, Text, View } from 'react-native';
 import { styles } from '../../styles';
 import { StatsTopBox } from '../../components/StatsTopBox/StatsTopBox';
+import { ProgressChart } from 'react-native-chart-kit';
+import { getCategoryTotals } from '../../db/db';
+
 
 export const StatsPage = () => {
+
+    const [data, setData] = useState([]);
+
+     useFocusEffect(
+            useCallback(() => {
+
+                const fetchData = async () => {
+                    const expencesDatas = getCategoryTotals();
+                    setData(expencesDatas);
+                }
+                fetchData
+                
+            }, [data])
+        );   
+    
+
+
     return(
         <View style={styles.stats_container}>
             <View style={styles.stats_header}>
@@ -34,6 +55,15 @@ export const StatsPage = () => {
                             <Text style={{fontSize: 16, letterSpacing: 0.5}}>
                                 Kategorilere göre harcamaların
                             </Text>
+                            <ProgressChart
+                                data={data}
+                                width={"100%"}
+                                height={220}
+                                strokeWidth={16}
+                                radius={32}
+                                chartConfig={chartConfig}
+                                hideLegend={false}
+                            />
                         </View>
                     </View>
                     <View style={styles.spending_by_category}>
